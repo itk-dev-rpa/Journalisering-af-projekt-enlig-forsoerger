4517bdb373c9363fb9018d14ce8058106a24a450
Mathias G <ghbm@aarhus.dk>
2026-04-28 11:00:48 +0200

    job id

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -184,5 +184,5 @@ def attach_email_to_case(email: Email, case: NovaCase, graph_access: GraphAccess
 if __name__ == '__main__':
     conn_string = os.getenv("OpenOrchestratorConnString")
     crypto_key = os.getenv("OpenOrchestratorKey")
-    oc = OrchestratorConnection("Enlig forsørger test", conn_string, crypto_key, "", "")
+    oc = OrchestratorConnection("Enlig forsørger test", conn_string, crypto_key, "", "", "")
     process(oc)
